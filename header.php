0f62e4c774e524feb5a331dc64aad4ab1def930a
developersazon <developer.sazon@gmail.com>
2023-07-22 23:35:29 +0600

    Change all link for WordPres

--- header.php
+++ header.php
@@ -13,28 +13,28 @@
     <header class="">
       <nav class="navbar navbar-expand-lg">
         <div class="container">
-          <a class="navbar-brand" href="index.html"><h2>Stand Blog<em>.</em></h2></a>
+          <a class="navbar-brand" href="index.php"><h2>Stand Blog<em>.</em></h2></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
               <li class="nav-item active">
-                <a class="nav-link" href="index.html">Home
+                <a class="nav-link" href="index.php">Home
                   <span class="sr-only">(current)</span>
                 </a>
               </li> 
               <li class="nav-item">
-                <a class="nav-link" href="about.php">About Us</a>
+                <a class="nav-link" href="<?php bloginfo('template_directory') ?>/about.php">About Us</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="blog.php">Blog Entries</a>
+                <a class="nav-link" href="<?php bloginfo('template_directory') ?>/blog.php">Blog Entries</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="post-details.php">Post Details</a>
+                <a class="nav-link" href="<?php bloginfo('template_directory') ?>/post-details.php">Post Details</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="contact.php">Contact Us</a>
+                <a class="nav-link" href="<?php bloginfo('template_directory') ?>/contact.php">Contact Us</a>
               </li>
             </ul>
           </div>
